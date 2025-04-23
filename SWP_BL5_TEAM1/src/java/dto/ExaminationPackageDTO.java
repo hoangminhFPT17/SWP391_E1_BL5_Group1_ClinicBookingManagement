92c32bcea9bd495cfc747577169a9620d32cb2a4
KietTT <kiettthe170577@fpt.edu.vn>
2025-04-24 01:38:02 +0700

    complete service manager

--- SWP_BL5_TEAM1/src/java/dto/ExaminationPackageDTO.java
+++ SWP_BL5_TEAM1/src/java/dto/ExaminationPackageDTO.java
@@ -16,76 +16,70 @@ public class ExaminationPackageDTO {
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
      */
-
     /**
      *
      * @author LENOVO
      */
-    public class ExaminationPackage {
+    private int packageId;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String specialty;
 
-        private int packageId;
-        private String name;
-        private String description;
-        private BigDecimal price;
-        private String specialty;
+    public ExaminationPackageDTO() {
+    }
 
-        public ExaminationPackage() {
-        }
+    public ExaminationPackageDTO(int packageId, String name, String description, BigDecimal price, String specialty) {
+        this.packageId = packageId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.specialty = specialty;
+    }
 
-        public ExaminationPackage(int packageId, String name, String description, BigDecimal price, String specialty) {
-            this.packageId = packageId;
-            this.name = name;
-            this.description = description;
-            this.price = price;
-            this.specialty = specialty;
-        }
+    public int getPackageId() {
+        return packageId;
+    }
 
-        public int getPackageId() {
-            return packageId;
-        }
+    public void setPackageId(int packageId) {
+        this.packageId = packageId;
+    }
 
-        public void setPackageId(int packageId) {
-            this.packageId = packageId;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public String getName() {
-            return name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    public String getDescription() {
+        return description;
+    }
 
-        public String getDescription() {
-            return description;
-        }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-        public void setDescription(String description) {
-            this.description = description;
-        }
+    public BigDecimal getPrice() {
+        return price;
+    }
 
-        public BigDecimal getPrice() {
-            return price;
-        }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 
-        public void setPrice(BigDecimal price) {
-            this.price = price;
-        }
+    public String getSpecialty() {
+        return specialty;
+    }
 
-        public String getSpecialty() {
-            return specialty;
-        }
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
 
-        public void setSpecialty(String specialty) {
-            this.specialty = specialty;
-        }
-        
-        
-
-        @Override
-        public String toString() {
-            return "ExaminationPackage{" + "packageId=" + packageId + ", name=" + name + ", description=" + description + ", price=" + price + ", specialtyId=" + specialty + '}';
-        }
+    @Override
+    public String toString() {
+        return "ExaminationPackage{" + "packageId=" + packageId + ", name=" + name + ", description=" + description + ", price=" + price + ", specialtyId=" + specialty + '}';
     }
 
 }
