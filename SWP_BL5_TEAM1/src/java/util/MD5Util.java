e9b918dd5d70bf773ac542119bf64c12964347de
HoangMinh <minhnhhe172293@fpt.edu.vn>
2025-04-28 11:56:24 +0700

    small fixes

--- SWP_BL5_TEAM1/src/java/util/MD5Util.java
+++ SWP_BL5_TEAM1/src/java/util/MD5Util.java
@@ -34,6 +34,6 @@ public class MD5Util {
     
     
      public static void main(String[] args) {
-     System.err.print(getMD5Hash("12345678"));
+     System.err.print(getMD5Hash("12345"));
      }
 }
